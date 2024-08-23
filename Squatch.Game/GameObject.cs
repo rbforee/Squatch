c814e267de9b83e14cebda21df6580479b24468c
rbforee <robert.foree@gmail.com>
2024-08-23 00:43:32 -0400

    new files added

---
+++ Squatch.Game/GameObject.cs
@@ -0,0 +1,12 @@
+using Raylib_cs;
+
+
+namespace Squatch.Game;
+
+public class GameObject
+{
+    public int x {get;set;}
+    public int y {get;set;}
+    public Color color {get;set;}
+    public int radius {get;set;}
+}
---
+++ Squatch.Game/Program.cs
@@ -0,0 +1,59 @@
+﻿using Raylib_cs;
+
+namespace Squatch.Game;
+
+class Program
+{
+    static List<GameObject> GameObjectList;
+    static int ScreenHeight;
+    static int ScreenWidth;
+
+    static void Main(string[] args)
+    {
+
+        CreateObjects();
+
+        ScreenHeight = 480;
+        ScreenWidth = 800;
+
+        Raylib.InitWindow(ScreenWidth, ScreenHeight, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            foreach(var i in GameObjectList)
+            {
+                Raylib.DrawCircle(i.x, i.y, i.radius, i.color);
+            }
+            
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+
+
+    static void CreateObjects()
+    {
+        GameObjectList = new List<GameObject>();
+
+        var rnd = new Random((int)DateTime.Now.Ticks % int.MaxValue );
+
+
+        for(var i = 0; i < 50; i++)
+        {
+            GameObjectList.Add
+            (
+                new GameObject 
+                {
+                    x = rnd.Next() % 800, 
+                    y = rnd.Next() % 480,
+                    color = Raylib.ColorFromHSV(rnd.Next() % 360, .75f, .5f),
+                    radius = rnd.Next() % 50    
+                }
+            );
+        }
+    }
+}
